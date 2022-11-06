b0477d25369653369d223f450beb584a0e5ecf7c
Syed Ajmathulla <84552376+Ajju1197@users.noreply.github.com>
2022-11-06 22:25:17 +0530

    Adding Scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './Animations/animation';
 
@@ -13,6 +13,34 @@ import { slideInAnimation } from './Animations/animation';
 })
 export class AppComponent implements OnInit {
   title = 'Angular-Crud-With-Api-App';
+  isShow: boolean;
+  topPosToStartShowing = 100;
+
+  @HostBinding('window:scroll')
+  
+  checkScroll() {
+      
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+  
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior:'smooth'
+    })
+  }
 
   ngOnInit(): void {
     window.addEventListener('scroll', function () {
